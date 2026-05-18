9278519554e226367c51bc7a33aab1e6414061b9
Rodrigo Henrique <150493553+TucanoiDEV@users.noreply.github.com>
2026-05-18 14:51:35 -0300

    scenary upgrade

---
+++ Frogger/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int speed;
+    private Rigidbody2D playerRb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, speed, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position -= new Vector3(0, speed, 0);
+        }
+    }
+}
