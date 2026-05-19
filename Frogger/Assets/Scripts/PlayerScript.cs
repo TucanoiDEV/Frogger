864c35d35db3de606d6099a933d03258b0660fba
Rodrigo Henrique <150493553+TucanoiDEV@users.noreply.github.com>
2026-05-19 15:06:44 -0300

    add player rotation

--- Frogger/Assets/Scripts/PlayerScript.cs
+++ Frogger/Assets/Scripts/PlayerScript.cs
@@ -21,18 +21,22 @@ public class PlayerScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, -90);
             transform.position += new Vector3(speed, 0, 0);
         }
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
             transform.position -= new Vector3(speed, 0, 0);
         }
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
             transform.position += new Vector3(0, speed, 0);
         }
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
             transform.position -= new Vector3(0, speed, 0);
         }
     }
