acead36d033d70870caf2dc4a0fef478a3ce89f7
Rodrigo Henrique <150493553+TucanoiDEV@users.noreply.github.com>
2026-05-20 12:10:21 -0300

    gameplay conclusion

--- Frogger/Assets/Scripts/PlayerScript.cs
+++ Frogger/Assets/Scripts/PlayerScript.cs
@@ -9,6 +9,9 @@ public class PlayerScript : MonoBehaviour
 
     private Animator animator;
 
+    [SerializeField] private bool isSafe;
+    [SerializeField] private bool isDead = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,38 +25,66 @@ public class PlayerScript : MonoBehaviour
 
     void Movement()
     {
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        if(speed == 1)
         {
-            animator.SetTrigger("walkTrigger");
-            transform.rotation = Quaternion.Euler(0, 0, -90);
-            transform.position += new Vector3(speed, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            animator.SetTrigger("walkTrigger");
-            transform.rotation = Quaternion.Euler(0, 0, 90);
-            transform.position -= new Vector3(speed, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            animator.SetTrigger("walkTrigger");
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            transform.position += new Vector3(0, speed, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            animator.SetTrigger("walkTrigger");
-            transform.rotation = Quaternion.Euler(0, 0, 180);
-            transform.position -= new Vector3(0, speed, 0);
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                animator.SetTrigger("walkTrigger");
+                transform.rotation = Quaternion.Euler(0, 0, -90);
+                transform.position += new Vector3(speed, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                animator.SetTrigger("walkTrigger");
+                transform.rotation = Quaternion.Euler(0, 0, 90);
+                transform.position -= new Vector3(speed, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                animator.SetTrigger("walkTrigger");
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                transform.position += new Vector3(0, speed, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                animator.SetTrigger("walkTrigger");
+                transform.rotation = Quaternion.Euler(0, 0, 180);
+                transform.position -= new Vector3(0, speed, 0);
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D playerCollision)
     {
-        if (playerCollision.gameObject.CompareTag("Enemy"))
+        if (playerCollision.gameObject.CompareTag("Log"))
+        {
+            isSafe = true;
+        }
+
+        if (playerCollision.gameObject.CompareTag("Enemy") && isSafe == false)
         {
             animator.SetTrigger("deathTrigger");
         }
+    }
+    private void OnCollisionExit2D(Collision2D playerCollision)
+    {
+       isSafe = false;
+    }
+    private void OnTriggerStay2D(Collider2D playerCollision)
+    {
+        if(playerCollision.CompareTag("Water"))
+        {
+            if(isSafe == false && isDead == false)
+            {
+                isDead = true;
+                animator.SetTrigger("deathTrigger");
+            }
+        }
+    }
+
+    void Stop()
+    {
+        speed = 0;
     }
 
     void Death()
